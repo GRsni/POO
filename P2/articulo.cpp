0826d58949bae1d387671734d326f02007d8740d
GRsni <santiagojesus.mas@gmail.com>
2020-04-16 01:47:47 +0200

    started work on P2, finished articulo class

---
+++ P2/articulo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+#include "articulo.hpp"
+
+Articulo::Articulo(const double ref, const Cadena &C, const Fecha &F, double precio, double stock) : referencia_(ref),
+                                                                                                     titulo_(C),
+                                                                                                     f_publi_(F),
+                                                                                                     precio_(precio),
+                                                                                                     stock_(stock) {}
+
+Articulo::Articulo(const Articulo &a) : referencia_(a.referencia()),
+                                        titulo_(a.titulo()),
+                                        f_publi_(a.f_publi()),
+                                        precio_(a.precio()),
+                                        stock_(a.stock()) {}
+
+const double Articulo::referencia() const { return referencia_; }
+const Cadena &Articulo::titulo() const { return titulo_; }
+const Fecha &Articulo::f_publi() const { return f_publi_; }
+const double Articulo::precio() const { return precio_; }
+double &Articulo::precio() { return precio_; }
+const double Articulo::stock() const { return stock_; }
+double &Articulo::stock() { return stock_; }
+
+Articulo::~Articulo() {}
+
+std::ostream &operator<<(std::ostream &out, Articulo &A)
+{
+    out << std::setprecision(2)
+        << "[" << A.referencia() << "] \"" << A.titulo() << "\", " << A.f_publi().anno() << ". "
+        << std::fixed << A.precio() << " \u20AC."
+        << std::defaultfloat;
+    return out;
+}
---
+++ P2/articulo.hpp
@@ -0,0 +1,36 @@
+#ifndef ARTICULO_H
+#define ARTICULO_H
+
+#include <iostream>
+#include "../P1/cadena.hpp"
+#include "../P1/fecha.hpp"
+
+class Articulo
+{
+public:
+    Articulo(const double ref, const Cadena &c, const Fecha &f, double precio, double stock = 0);
+    Articulo(const Articulo &A);
+
+    Articulo &operator=(const Articulo &A);
+
+    const double referencia() const;
+    const Cadena &titulo() const;
+    const Fecha &f_publi() const;
+    const double precio() const;
+    double &precio();
+    const double stock() const;
+    double &stock();
+
+    ~Articulo();
+
+private:
+    const double referencia_;
+    const Cadena titulo_;
+    const Fecha f_publi_;
+    double precio_;
+    double stock_;
+};
+
+std::ostream &operator<<(std::ostream &in, Articulo &A);
+
+#endif //ARTICULO_H
